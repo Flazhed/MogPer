175986947f4436b893e85452d25fe192c20900d0
Soren <soren@soren.soren>
2016-10-26 15:18:26 +0200

    Added first tests with setup

---
+++ src/test/java/mogper/test/AdminInterfaceTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mogper.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static mogper.test.BackendHolder.*;
+import org.junit.BeforeClass;
+/**
+ *
+ * @author Soren
+ */
+public class AdminInterfaceTest {
+    
+    
+    @BeforeClass
+    public void setup(){
+        adminInterface = null;
+    }
+    
+}
